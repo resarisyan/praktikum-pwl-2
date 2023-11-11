74fb15b05852cc6b154f5fb0fd790d04e35e98cf
LABTIF <labtif.unsur@gmail.com>
2023-11-11 14:08:39 +0700

    add index to controller

--- app/Http/Controllers/BookshelfController.php
+++ app/Http/Controllers/BookshelfController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bookshelf;
 use Illuminate\Http\Request;
 
 class BookshelfController extends Controller
 {
-    //
+    public function index(){
+        $data['bookshelves'] = Bookshelf::all();
+        dd($data);
+    }
 }
